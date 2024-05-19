ac18501c2f4401855b28b647527037cac7a974ef
JoaldoSanth <joaldosant3@gmail.com>
2024-05-19 02:44:27 -0300

    projeto no react

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Cronometro from './App.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Cronometro />
+  </React.StrictMode>,
+)
